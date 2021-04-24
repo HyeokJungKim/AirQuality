8105c48fcc32011a02d9d7c2781e60049b761114
Eric Kim <hyeokjungkim@gmail.com>
2021-04-24 19:16:56 -0400

    adds arrowhead

--- src/charts/graphs/windDirection.js
+++ src/charts/graphs/windDirection.js
@@ -16,14 +16,35 @@ class WindDirection extends React.Component{
             .attr("width", this.w)
             .attr("height", this.h)
 
-        // DEBUG: Make it into arrow
+        let arrowPoints = [
+            [0, 0],
+            [0, 20],
+            [20, 10]
+        ]
+
+        this.arrowHead = this.svg.append("svg:defs")
+            .append("marker")
+            .attr("id", "arrow")
+            .attr("markersUnit", "strokeWidth")
+            .attr("markersWidth", 20)
+            .attr("markerHeight", 20)
+            .attr("viewBox", [0, 0, 20, 20])
+            .attr("refX", 10)
+            .attr("refY", 10)
+            .attr("orient", "auto")
+            .append("path")
+            .attr("d", d3.line()(arrowPoints))
+            .attr("fill", "red")
+
         this.line = this.svg.append("line")
             .attr("x1", this.w/2)
             .attr("y1", this.h/2)
             .attr("x2", this.w/2)
-            .attr("y2", 0)
+            .attr("y2", 10)
             .attr("stroke", "red")
             .attr("stroke-width", 2)
+            .attr("marker-end", `url(#arrow)`)
+
 
         this.animateCompassDirection()
     }
