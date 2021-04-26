b984e437f48278874e90bb60eceb156df9bd7240
Eric Kim <hyeokjungkim@gmail.com>
2021-04-26 11:33:08 -0400

    https

--- src/adapters/server.js
+++ src/adapters/server.js
@@ -1,5 +1,5 @@
 function getCityDataAQ(lat, lon){
-    return fetch(`http://api.airvisual.com/v2/nearest_city?lat=${lat}&lon=${lon}&key=${process.env.REACT_APP_AIRVISUAL_API}`)
+    return fetch(`https://api.airvisual.com/v2/nearest_city?lat=${lat}&lon=${lon}&key=${process.env.REACT_APP_AIRVISUAL_API}`)
         .then(res => res.json())
   
 }
